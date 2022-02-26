78cbd81b9b15ae31e4674f3333343295f35a9d59
wolege <b1109955150@qq.com>
2022-02-26 18:22:48 +0800

    master

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include <vector>
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    std::vector<int> s;
+    for (int i = 0; i < 10; i++)
+    {
+        s.push_back(i);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout<<s[i]<<std::endl;
+    }
+    
 }
