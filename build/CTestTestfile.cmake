4e04a7211629e2e3e503d413957533bb696b1e6a
wolege <b1109955150@qq.com>
2022-02-26 18:18:05 +0800

    master

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lmj/code/gitTest
+# Build directory: /home/lmj/code/gitTest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
